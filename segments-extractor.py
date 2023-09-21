968cebdc3b313ad6fc17ec4597b8a8d2cddf722a
edenede2 <131803027+edenede2@users.noreply.github.com>
2023-09-21 13:18:45 +0300

    Update segments-extractor.py

--- segments-extractor.py
+++ segments-extractor.py
@@ -165,22 +165,22 @@ def main():
                 st.write("Log transformation has been reverted.")
             else:
                 st.warning("Unable to revert the log transformation. Please reload or re-upload the original data.")
-
-        all_full_subjects = data[data['Subjects'].str.startswith('Full_')]['Subjects'].unique().tolist()
-        all_segmented_subjects = data[~data['Subjects'].str.startswith('Full_')]['Subjects'].unique().tolist()
-        selected_segmented_subjects = st.multiselect('Select Segmented Subjects', all_segmented_subjects, default=all_segmented_subjects)
+         # If specific events are selected, display segments relevant to those events
+    # Filter segments based on selected subjects and events
+    filtered_data = data[(data['Subjects'].isin(selected_segmented_subjects)) & (data['Events'].isin(selected_events))]
+    relevant_segments = filtered_data['Segments'].unique().tolist()
+    selected_segments = relevant_segments
+    selected_segments = st.multiselect('Select Segments', relevant_segments, default=[s for s in selected_segments if s in relevant_segments])
+    filtered_data = filtered_data[filtered_data['Segments'].isin(selected_segments)]
+    
+    all_full_subjects = data[data['Subjects'].str.startswith('Full_')]['Subjects'].unique().tolist()
+    all_segmented_subjects = data[~data['Subjects'].str.startswith('Full_')]['Subjects'].unique().tolist()
+    selected_segmented_subjects = st.multiselect('Select Segmented Subjects', all_segmented_subjects, default=all_segmented_subjects)
 
     
-        all_events = data['Events'].unique().tolist()
-        selected_events = st.multiselect('Select Events', all_events, default=all_events)
+    all_events = data['Events'].unique().tolist()
+    selected_events = st.multiselect('Select Events', all_events, default=all_events)
     
-        # If specific events are selected, display segments relevant to those events
-         # Filter segments based on selected subjects and events
-        filtered_data = data[(data['Subjects'].isin(selected_segmented_subjects)) & (data['Events'].isin(selected_events))]
-        relevant_segments = filtered_data['Segments'].unique().tolist()
-        selected_segments = relevant_segments
-        selected_segments = st.multiselect('Select Segments', relevant_segments, default=[s for s in selected_segments if s in relevant_segments])
-        filtered_data = filtered_data[filtered_data['Segments'].isin(selected_segments)]
 
     measurements = ['RMSSD', 'SDNN','MHR']
     selected_measurements = st.selectbox('Select Measurements', measurements, index=0)
@@ -225,6 +225,10 @@ def main():
     
         full_data = data[data['Subjects'].str.startswith('Full_')]
         original_full_data = full_data.copy()
+
+        all_full_events = full_data['Events'].unique().tolist()
+        selected_full_events = st.multiselect('Select Events for Full Data', all_full_events, default=all_full_events)
+        filtered_full_data = full_data[full_data['Subjects'].isin(selected_full_subjects) & full_data['Events'].isin(selected_full_events)]
     
     full_measurements = ['RMSSD', 'SDNN', 'MHR']
     selected_full_subjects = st.multiselect('Select Full Subjects', all_full_subjects, default=all_full_subjects)
@@ -233,9 +237,7 @@ def main():
     full_plot_types = ["Box Plot", "Violin Plot", "Histogram", "Swarm Plot", "Plot Line"]
     selected_full_plot = st.selectbox('Select Visualization Type for Full Data', full_plot_types)
     
-    all_full_events = full_data['Events'].unique().tolist()
-    selected_full_events = st.multiselect('Select Events for Full Data', all_full_events, default=all_full_events)
-    filtered_full_data = full_data[full_data['Subjects'].isin(selected_full_subjects) & full_data['Events'].isin(selected_full_events)]
+    
 
     if st.button("Generate Full Data Plot"):
         # Depending on the type of plot selected, call the appropriate function
