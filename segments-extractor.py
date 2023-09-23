c6ad027873a05e0520e10d7bb084328d61d9940e
edenede2 <131803027+edenede2@users.noreply.github.com>
2023-09-24 01:36:41 +0300

    Update segments-extractor.py

--- segments-extractor.py
+++ segments-extractor.py
@@ -166,7 +166,7 @@ def resilience_sustainability_page():
     hrv_threshold = mean_hrv.mean()
 
     # Categorize subjects based on the HRV threshold
-    full_data['HRV_Category'] = mean_hrv.apply(lambda x: 'High' if x >= hrv_threshold else 'Low').reindex(full_data['Subjects']).reset_index(drop=True)
+    full_data.loc[:, 'HRV_Category'] = mean_hrv.apply(lambda x: 'High' if x >= hrv_threshold else 'Low').reindex(full_data['Subjects']).reset_index(drop=True)
 
     threshold = st.slider("Set Threshold for Highlighting Significant Change (%)", min_value=0, max_value=100, value=10)
     
@@ -180,7 +180,11 @@ def resilience_sustainability_page():
     change_sc2_sc1, change_sc3_sc1 = categorize_subjects(change_sc2_sc1, change_sc3_sc1, threshold)
     
     def plot_with_threshold(change_data, scenario, measurement, threshold):
-        # Define colors based on HRV_Category
+        # Check for duplicate subjects
+        duplicate_subjects = change_data[change_data.duplicated('Subjects', keep=False)]
+        if not duplicate_subjects.empty:
+            st.warning("Found duplicate subjects. Please check the data.")
+            st.write(duplicate_subjects)        # Define colors based on HRV_Category
         colors = full_data.set_index('Subjects')['HRV_Category'].map({'High': 'red', 'Low': 'blue'}).reindex(change_data['Subjects']).reset_index(drop=True)
         
         # Calculate the percentage of subjects under the threshold
